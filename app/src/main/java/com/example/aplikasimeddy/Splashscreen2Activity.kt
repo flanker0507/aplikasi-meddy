aa0a5639c5ead0d125f7815c89892f83eac85d60
mallvinorz <mallvino1216@gmail.com>
2023-05-25 08:40:27 +0700

    add splashscreen activity

---
+++ app/src/main/java/com/example/aplikasimeddy/Splashscreen2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasimeddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splashscreen2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen2)
+    }
+}
