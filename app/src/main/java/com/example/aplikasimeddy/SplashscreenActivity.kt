a4d9e6e5b470d70d2487644faab7eacb73c98331
mallvinorz <mallvino1216@gmail.com>
2023-05-25 12:24:36 +0700

    change onboarding 1 layout to constraint

--- app/src/main/java/com/example/aplikasimeddy/SplashscreenActivity.kt
+++ app/src/main/java/com/example/aplikasimeddy/SplashscreenActivity.kt
@@ -1,16 +1,18 @@
 package com.example.aplikasimeddy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
+@SuppressLint("CustomSplashScreen")
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
-        
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
